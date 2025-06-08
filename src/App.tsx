e2c534c6f4010267d7a8944149bf2980cf492dd6
Josh Kim <realx1212@gmail.com>
2025-06-09 00:45:03 +0900

    refactor: Clean up import statements and reorder components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Layout from './layout/Layout';
-import Hero from './components/sections/Hero';
-import About from './components/sections/About';
-import Special from './components/sections/Special';
-import Highlights from './components/sections/Highlights';
-import Schedule from './components/sections/Schedule';
-import Registration from './components/sections/Registration';
-import Testimonial from './components/sections/Testimonial';
-import Gallery from './components/sections/Gallery';
-import FAQ from './components/sections/FAQ';
-import FooterCTA from './components/sections/FooterCTA';
-import { LanguageProvider } from '@/contexts/LanguageContext';
+import Layout from "./layout/Layout";
+import Hero from "./components/sections/Hero";
+import About from "./components/sections/About";
+import Special from "./components/sections/Special";
+import Highlights from "./components/sections/Highlights";
+import Schedule from "./components/sections/Schedule";
+import Registration from "./components/sections/Registration";
+import Testimonial from "./components/sections/Testimonial";
+import Gallery from "./components/sections/Gallery";
+import FAQ from "./components/sections/FAQ";
+import FooterCTA from "./components/sections/FooterCTA";
+import { LanguageProvider } from "@/contexts/LanguageContext";
 
 function App() {
   return (
@@ -21,9 +20,9 @@ function App() {
         <Special />
         <Highlights />
         <Schedule />
-        <Gallery />
         <Registration />
         <Testimonial />
+        <Gallery />
         <FAQ />
         <FooterCTA />
       </Layout>
